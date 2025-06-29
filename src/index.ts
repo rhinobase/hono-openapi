9b1bdf713a69f539d893bca6999c29536feae793
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-06-29 10:34:47 +0530

    chore: renamed openapi file to handler

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { generateSpecs, openAPISpecs } from "./openapi.js";
+export { generateSpecs, openAPISpecs } from "./handler.js";
 export type * from "./types.js";
 export * from "./utils.js";
 export * from "./middlewares.js";
