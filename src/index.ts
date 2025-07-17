8a8a1bb7b59891a7bc6124331e5d495b7e70b4c9
Aditya Mathur <57684218+MathurAditya724@users.noreply.github.com>
2025-07-17 15:48:30 +0530

    chore: released rc 0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { generateSpecs } from "./handler.js";
-export * from "./middlewares.js";
+export { describeRoute, resolver, validator } from "./middlewares.js";
 export type * from "./types.js";
 export * from "./utils.js";
